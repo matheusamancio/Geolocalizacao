6ca21c1100861fa47e1e88a97d732e8a0bd81ffc
Kaique Damato <kaique_pantosi@hotmail.com>
2015-03-04 12:10:31 -0300

    feito a tela de login

---
+++ Geolocalizacao/Geolocalizacao/LoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.h
+//  Geolocalizacao
+//
+//  Created by Kaique Damato on 3/4/15.
+//  Copyright (c) 2015 Matheus Amancio Seixeiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIButton *singInButton;
+
+@end
